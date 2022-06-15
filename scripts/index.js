15088a399b122dd868ee4c952d3cc2a5e21fa749
Natalia Zaitceva <sfk89@yandex.ru>
2022-06-16 00:11:19 +0900

    feat: html, css, first popup

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+const profileeditbutton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.popup')
+
+profileeditbutton.addEventListener('click', (Event) => {
+    popup.classList.add('popup_opened')
+})
+
+const closebutton = document.querySelector('.popup__close')
+
+closebutton.addEventListener('click', (Event) => {
+    popup.classList.remove('popup_opened')
+})
+
+const popupSave = document.querySelector('.popup__submit')
+
+let formElement = document.querySelector('.popup__form')
+let profileInfo = document.querySelector('.profile__info')
+
+let nameInput = formElement.querySelector('.popup__name')
+let jobInput = formElement.querySelector('.popup__description')
+let profileName = profileInfo.querySelector('.profile__name');
+let profileProfession = profileInfo.querySelector('.profile__subtitle')
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileProfession.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
