473bf3e4a425336a5fb5fb45285a8b55482df7ae
Natalia Zaitceva <sfk89@yandex.ru>
2022-06-22 14:41:57 +0900

    fix: errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,10 @@
-const profileeditbutton = document.querySelector('.button_profile_edit')
+const profileeditbutton = document.querySelector('.button_view_edit')
 const popup = document.querySelector('.popup')
 
 profileeditbutton.addEventListener('click', openPopup);
 
-function openPopup(event){
-    popup.classList.add('popup_opened')
-}
 
-const closeButton = document.querySelector('.button_popup_close')
+const closeButton = document.querySelector('.button_view_close')
 
 function closePopup(event){
     popup.classList.remove('popup_opened')
@@ -18,33 +15,22 @@ closeButton.addEventListener('click', closePopup)
 
 let formElement = document.querySelector('.popup__form')
 let profileInfo = document.querySelector('.profile__info')
-let popupButtonSave = document.querySelector('.button_popup_submit')
-
-function closePopup(event){ 
-    popup.classList.remove('popup_opened')
-}
-
-function popupSave(event){
- popupButtonSave.addEventListener('click', function
-closePopup(event){ 
-    popup.classList.remove('popup_opened')
-})     
-}
-
-popupButtonSave.addEventListener('click', closePopup);
 
 let nameInput = formElement.querySelector('.popup__input_description_name')
 let jobInput = formElement.querySelector('.popup__input_description_profession')
 let profileName = profileInfo.querySelector('.profile__name');
 let profileProfession = profileInfo.querySelector('.profile__subtitle')
 
+function openPopup(event){
+nameInput = formElement.querySelector('.popup__input_description_name')
+jobInput = formElement.querySelector('.popup__input_description_profession')
+    popup.classList.add('popup_opened')
+}
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileProfession.textContent = jobInput.value;
- function closePopup(event){ 
-    popup.classList.remove('popup_opened')
-}   
+ closePopup()
 }
 
 
